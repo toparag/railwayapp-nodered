e5882a72b18d8cf0b1a174dea88b0db223c4879b
Parag Patel <toparag@gmail.com>
2025-08-03 21:22:03 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Entrypoint: Checking /data/settings.js..."
+
+if [ ! -f /data/settings.js ]; then
+  echo "No settings.js found in /data. Copying from /app..."
+  cp /app/settings.js /data/settings.js
+  chmod 644 /data/settings.js
+else
+  echo "settings.js already exists in /data."
+fi
+
+exec npm start
